6f70dccfbf57e01e01b7a2703a76662f262c1f04
angel <158207757+acabreramata@users.noreply.github.com>
2024-08-19 22:09:21 -0600

    funcion capturar datos test

--- EasyPlay/src/main/resources/static/js/scripts.js
+++ EasyPlay/src/main/resources/static/js/scripts.js
@@ -1,3 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
+console.log("hola");
 
+function capturarDatos() {
+    // capturar los valores de los inputs
+    const inputsAccion = document.getElementsByClassName('input-box');
+    // recorrer los inputs e imprimirlos
+    for (const input of inputsAccion) {
+        console.log(input.value);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed');
+    const botonRFID = document.getElementById('botonRFID');
+    if (botonRFID) {
+        botonRFID.addEventListener('click', capturarDatos);
+    } else {
+        console.error('El botón RFID no se encontró.');
+    }
 });
